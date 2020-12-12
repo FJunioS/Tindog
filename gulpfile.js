f21f0bf47e2d735ffd34f4120ef8514540e29eda
Junio Koi <juniokoi@hotmail.com>
2020-12-12 12:10:01 -0300

    Removido partes que não entendia muito bem do código

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,12 @@
-const {src, dest, watch, series} = require('gulp');
+const gulp = require('gulp');
 const sass  = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const terser = require('gulp-terser');
-const { SourceMap } = require('module');
 const browsersync = require('browser-sync').create();
 
-// sass task.
-function scssTask(){
-    return src('app/scss/style.scss',{ sourcemaps: true}) 
-    .pipe (sass())
-    .pipe(postcss([cssnano]))
-    .pipe(dest('dist', {sourcemaps:'.'}))
-};
-
-// Javascript task
-function jsTask(){
-    return src('app/js/script.js', {sourcemaps:true})
-        .pipe(terser())
-        .pipe(dest('dist', {sourcemaps:'.'}));
+//Compile scss into css
+function style() {
+    return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browsersync.stream());
 }
-
-// Browsersync Tasks
-function browsersyncServe(cb){
-    browsersync.init({
-        server: {
-        baseDir: '.' 
-    }});
-    cb();
-}
-function browsersyncReload(cb){
-    browsersync.reload();
-    cb();
-}
-
-//Watch Task
-function watchTask(){
-    watch('*.html', browsersyncReload);
-    watch(['app/scss/**/*scss', 'app/js/**/*.js'], series (scssTask, jsTask, browsersyncReload));
-}
-// Default Gulp task
-exports.default = series (
-    scssTask,
-    jsTask,
-    browsersyncServe,
-    watchTask
-);
+exports.style = style;
