09f75f947fcf195d612a7213711387c128db3f5b
Junio Koi <juniokoi@hotmail.com>
2020-12-12 13:09:47 -0300

    Descobri como que usa o Bootstrap e scss juntos, agora folta concertar o diacho do gulp dnv

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 const gulp = require('gulp');
-const sass  = require('gulp-sass');
-const browsersync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 //Compile scss into css
 function style() {
     return gulp.src('src/scss/**/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('src/css'))
-    .pipe(browsersync.stream());
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
 }
+
+
+//BrowserSync config
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./src",
+            index: "/index.html"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style)
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('src/scss/**/*.js').on('change', browserSync.reload);
+}
+
 exports.style = style;
+exports.watch = watch;
